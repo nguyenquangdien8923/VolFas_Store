9cc7085a122309bc308a81202ce17ac05554fdce
Nguyen Quang Dien <quangdien0809@gmail.com>
2024-07-27 07:38:50 +0700

    Spring initializr the project

---
+++ VolFas_Store/src/main/java/com/devjdib/VolFas_Store/VolFasStoreApplication.java
@@ -0,0 +1,13 @@
+package com.devjdib.VolFas_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolFasStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolFasStoreApplication.class, args);
+	}
+
+}
